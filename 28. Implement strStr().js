49b9e734e20f0b16e53a998fa611081374dcc681
Kairat Batyrbayev <k.batyrbayev>
2021-08-23 09:29:03 +0600

    28. Implement strStr()

---
+++ 28. Implement strStr().js
@@ -0,0 +1,7 @@
+var strStr = function(haystack, needle) {
+    return haystack.indexOf(needle);
+};
+
+console.log(strStr("hello", "ll"));
+console.log(strStr(haystack = "aaaaa", needle = "bba"));
+console.log(strStr(haystack = "", needle = ""));
